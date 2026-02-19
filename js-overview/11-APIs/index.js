e67d9d0e126ba6c6317ba3ea752e8d955eba5013
Ankit Mittal <191123870+AnkitnMittal@users.noreply.github.com>
2026-02-19 12:34:51 +0000

    learn async/await method to simplify promises

--- js-overview/11-APIs/index.js
+++ js-overview/11-APIs/index.js
@@ -11,6 +11,7 @@ if (!apikey) {
 
 const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/India/today?unitGroup=metric&elements=datetime%2CdatetimeEpoch%2Cdew%2Cfeelslike%2Cfeelslikemax%2Cfeelslikemin%2Cname%2Coffset%2CsunriseEpoch%2CsunsetEpoch%2Ctemp%2Ctempmax%2Ctempmin&include=current&key=${apikey}&contentType=json`;
 
+/* ---------- Using Promises ---------- */
 fetch(url)
   .then((response) => {
     if (!response.ok) {
@@ -24,3 +25,18 @@ fetch(url)
   .catch((error) => {
     console.error("Error fetching weather data:", error);
   });
+
+/* ---------- Using Async/Await ---------- */
+async function fetchWeather() {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    console.log("Weather data:", data);
+  } catch (error) {
+    console.error(error);
+  }
+}
